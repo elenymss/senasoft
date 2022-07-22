aa48ea521696237f62e5c15f455d6dc91d4cb15c
ems1025 <emsancho@misena.edu.co>
2022-07-22 18:27:41 -0500

    segundo repo

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('registro',RegistroController::class);
+Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
